61b38bd491e754db51a8d4f98febc12ce93e2b1b
Ishak BELAHMAR <ishak@nytrio.com>
2020-01-11 08:08:38 -0800

    Handle 2-way sync when files are modified

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     version=version,
     packages=find_packages(),
     requires=['python (>= 2.7.6)'],
-    install_requires=['requests', 'lxml'],
+    install_requires=['requests', 'lxml', 'python-dateutil'],
     scripts=['wdc'],
     test_suite='tests',
     tests_require=['pytest'],
