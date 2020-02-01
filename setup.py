eced3b5fe5d9777b9f7899d6732c643008caab5e
Evgeny Ezhov <ezhov.evgeny@gmail.com>
2020-01-31 21:02:43 -0800

    Bump version to 3.14 and prepare to release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install as InstallCommand
 from setuptools.command.test import test as TestCommand
 
-version = "0.14"
+version = "3.14"
 requirements = "libxml2-dev libxslt-dev python-dev"
 
 
